bef59c0893693f557571684075715af726f273bd
Marlo Haering <haering@informatik.uni-hamburg.de>
2019-04-26 15:47:41 +0200

    minor style changes

--- frontend-demo/src/components/CommentList.vue
+++ frontend-demo/src/components/CommentList.vue
@@ -2,10 +2,17 @@
   <div>
     <h3>Comment List</h3>
     <button v-on:click="fetchComments()">Load placeholder comments</button>
-    <div v-for="comment in allComments" :key="comment.id" class="comment">
-      Name: {{comment.name}}
-      Email: {{comment.email}}
-      Body: {{comment.body}}
+
+    <div class="container">
+      <div v-for="comment in allComments" :key="comment.id" class="comment">
+        <div>
+          <b>{{comment.name}}</b>
+          <div>
+            <a :href="'mailto:'+comment.email" target="_top">{{comment.email}}</a>
+          </div>
+        </div>
+        <div>{{comment.body}}</div>
+      </div>
     </div>
   </div>
 </template>
@@ -24,8 +31,14 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
 .comment {
-  padding: 20px;
+  flex: 1 0 21%;
+  padding: 10px;
   border: 1px solid #ddd;
 }
 </style>
