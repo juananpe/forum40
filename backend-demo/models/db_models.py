fd22e465c5e65253635a389d64e6e3444c719a29
Jakob Andersen <jakob.andersen@haw-hamburg.de>
2019-06-10 18:59:54 +0200

    fix model

--- backend-demo/models/db_models.py
+++ backend-demo/models/db_models.py
@@ -23,8 +23,11 @@ comments_model = api.model('Q', {
 
 comments_parser_sl = reqparse.RequestParser()
 comments_parser_sl.add_argument('label', action='append')
+comments_parser_sl.add_argument('keyword', action='append')
 comments_parser_sl.add_argument('skip', type=int, default=0, required=True)
 comments_parser_sl.add_argument('limit', type=int, default=50, required=True)
 
 comments_parser = reqparse.RequestParser()
 comments_parser.add_argument('label', action='append')
+comments_parser.add_argument('keyword', action='append')
+
