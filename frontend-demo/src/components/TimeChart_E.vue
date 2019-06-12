bb6294ef2dbd75ff0d6baef42757da8a693a9a5f
Marlo Haering <haering@informatik.uni-hamburg.de>
2019-06-12 15:30:51 +0200

    responsive width

--- frontend-demo/src/components/TimeChart_E.vue
+++ frontend-demo/src/components/TimeChart_E.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        <v-chart :options="chart_options" />
-    </div>
+  <div>
+    <v-chart :options="chart_options" :autoresize="true"/>
+  </div>
 </template>
 
 <style>
@@ -11,86 +11,86 @@
  * don't forget to provide a size for the container).
  */
 .echarts {
-  width: 100;
+  width: 100%;
   height: 100;
 }
 </style>
 
 <script>
-import ECharts from 'vue-echarts'
-import 'echarts/lib/chart/bar'
-import 'echarts/lib/component/tooltip'
-import 'echarts/lib/component/legend'
+import ECharts from "vue-echarts";
+import "echarts/lib/chart/bar";
+import "echarts/lib/component/tooltip";
+import "echarts/lib/component/legend";
 
 export default {
   components: {
-    'v-chart': ECharts
+    "v-chart": ECharts
   },
-  methods : {
-
-  },
-  data () {
+  methods: {},
+  data() {
     var xAxisData = [];
     var data1 = [];
     var data2 = [];
     for (var i = 0; i < 100; i++) {
-        xAxisData.push(i);
-        data1.push(60 + (Math.sin(i / 5) * (i / 5 -10) + i / 6) * 5);
-        data2.push(60 + (Math.cos(i / 5) * (i / 5 -10) + i / 6) * 5);
+      xAxisData.push(i);
+      data1.push(60 + (Math.sin(i / 5) * (i / 5 - 10) + i / 6) * 5);
+      data2.push(60 + (Math.cos(i / 5) * (i / 5 - 10) + i / 6) * 5);
     }
     return {
-        series : [],
-        chart_options : {
-            title: {
-                text: '柱状图动画延迟'
+      series: [],
+      chart_options: {
+        title: {
+          text: "柱状图动画延迟"
+        },
+        legend: {
+          data: ["bar", "bar2"],
+          align: "left"
+        },
+        toolbox: {
+          feature: {
+            magicType: {
+              type: ["stack", "tiled"]
             },
-            legend: {
-                data: ['bar', 'bar2'],
-                align: 'left'
-            },
-            toolbox: {
-                feature: {
-                    magicType: {
-                        type: ['stack', 'tiled']
-                    },
-                    dataView: {},
-                    saveAsImage: {
-                        pixelRatio: 2
-                    }
-                }
-            },
-            tooltip: {
-                trigger: 'axis',
-            },
-            xAxis: {
-                data: xAxisData,
-                silent: false,
-                splitLine: {
-                    show: false
-                }
-            },
-            yAxis: {
-            },
-            series: [{
-                name: 'offtopic',
-                type: 'bar',
-                data: data1,
-                animationDelay: function (idx) {
-                    return idx * 10;
-                }
-            }, {
-                name: 'inappropriate',
-                type: 'bar',
-                data: data2,
-                animationDelay: function (idx) {
-                    return idx * 10 + 100;
-                }
-            }],
-            animationDelayUpdate: function (idx) {
-                return idx * 5;
+            dataView: {},
+            saveAsImage: {
+              pixelRatio: 2
             }
+          }
+        },
+        tooltip: {
+          trigger: "axis"
+        },
+        xAxis: {
+          data: xAxisData,
+          silent: false,
+          splitLine: {
+            show: false
+          }
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "offtopic",
+            type: "bar",
+            data: data1,
+            animationDelay: function(idx) {
+              return idx * 10;
+            }
+          },
+          {
+            name: "inappropriate",
+            type: "bar",
+            data: data2,
+            animationDelay: function(idx) {
+              return idx * 10 + 100;
+            }
+          }
+        ],
+        animationDelayUpdate: function(idx) {
+          return idx * 5;
         }
-    }
+      }
+    };
   }
-}
+};
 </script>
