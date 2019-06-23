e5516ae2bb1511b93a05d77d028a0a5b0e50371d
Jakob Andersen <jakob.andersen@haw-hamburg.de>
2019-06-23 14:52:17 +0200

    remove unused RequestParser

--- backend-demo/models/db_models.py
+++ backend-demo/models/db_models.py
@@ -1,42 +1,8 @@
-from apis.db import api
-from flask_restplus import fields
 from flask_restplus import reqparse
-
-obj_model = api.model('Object', { })
-
-aggregate_model = api.model('Aggregate', {
-    'pipeline': fields.List(fields.Nested(obj_model)),
-    'options': fields.Nested(obj_model),
-})
-
-label_time_model = api.model('Q', {
-    'name': fields.String(default='personalstories'),
-    'time_intervall': fields.Integer(default=36000000),
-})
 
 groupByModel = reqparse.RequestParser()
 groupByModel.add_argument('label')
 groupByModel.add_argument('keyword', action='append')
-
-timeseries_parser_all = reqparse.RequestParser()
-timeseries_parser_all.add_argument('time_intervall', type=int, default=36000000, required=True)
-
-timeseries_parser = reqparse.RequestParser()
-timeseries_parser.add_argument('label', action='append')
-timeseries_parser.add_argument('time_intervall', type=int, default=36000000, required=True)
-
-
-
-
-timeseries_parser_single = reqparse.RequestParser()
-timeseries_parser_single.add_argument('label', required=True)
-timeseries_parser_single.add_argument('time_intervall', type=int, default=36000000, required=True)
-
-comments_model = api.model('Q', {
-    'labelName': fields.String(default='personalstories'),
-    'skip': fields.Integer(default=1),
-    'limit': fields.Integer(default=50)
-})
 
 comments_parser_sl = reqparse.RequestParser()
 comments_parser_sl.add_argument('label', action='append')
