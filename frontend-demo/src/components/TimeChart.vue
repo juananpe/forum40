31eeed2292a4ee369928c26db8a1473a89d2d101
Jakob Andersen <jakob.andersen@haw-hamburg.de>
2019-07-14 19:23:57 +0200

    remove d3 chart

--- frontend-demo/src/components/TimeChart.vue
+++ frontend-demo/src/components/TimeChart.vue
@@ -1,71 +1,217 @@
 <template>
-  <div class="timechart">
-    <h3>Kommentarverteilung</h3>
-    <div id="streamgraph"></div>
+  <div>
+    <v-chart :options="chart_options" :autoresize="true"/>
   </div>
 </template>
 
+<style>
+/**
+ * The default size is 600px×400px, for responsive charts
+ * you may need to set percentage values as follows (also
+ * don't forget to provide a size for the container).
+ */
+.echarts {
+  width: 100%;
+  height: 100;
+}
+</style>
+
 <script>
 import Service, { Endpoint } from "../api/db";
-import * as d3 from "d3";
-import streamgraph from "../charts/streamgraph";
 
 import { Getters } from "../store/const";
 import { mapGetters } from "vuex";
 
+import ECharts from "vue-echarts";
+import "echarts/lib/chart/bar";
+import "echarts/lib/component/tooltip";
+import "echarts/lib/component/legend";
+import "echarts/lib/component/dataZoom";
+
 export default {
-  name: "TimeChart",
-  data: function() {
-    return {
-      id: null,
-      data: [],
-      chart: streamgraph()
-    };
+  components: {
+    "v-chart": ECharts
   },
-  computed: {
-    ...mapGetters([Getters.selectedLabels]),
-    playload_time_list: function() {
-      return {
-        name: this[Getters.selectedLabels][0],
-        time_intervall: 120000000
-      };
-    }
+    computed: {
+    ...mapGetters([
+        Getters.selectedLabels, 
+        Getters.keywordfilter,
+        Getters.timeFrequency]),
+
   },
-  mounted: function() {
-    if (this[Getters.selectedLabels].length > 0) this.getData();
-  },
-  watch: {
-    selectedLabels() {
-      if (this[Getters.selectedLabels].length > 0) this.getData();
+   watch: {
+    timeFrequency() {
+      this.updateChart()
     },
-    data: function() {
-      this.drawChart();
+    selectedLabels() {
+      if (this[Getters.selectedLabels].length > 0) this.updateChart_OnLabelChange(); else this.resetChartToOrigin()
+    },
+    keywordfilter() {
+      this.updateChart()
     }
+  },
+  mounted: async function() {
+    var p1 = this.initChart()
+    await Promise.all([p1]);
+    if (this[Getters.selectedLabels].length > 0) this.updateChart_OnLabelChange(); else this.resetChartToOrigin();
   },
   methods: {
-    getData: async function() {
-      const { data } = await Service.post(
-        Endpoint.TIMESERIES,
-        this.playload_time_list
-      );
-      data.forEach(d => {
-        d["time"] = new Date(d["_id"]);
-        delete d["_id"];
-        delete d["sum"];
-      });
-      this.data = data;
+    initChart: async function() {
+      const { data } = await Service.get(Endpoint.LABELS);
+      data.labels.push("Gesamtheit")
+    
+      data.labels.forEach(labelName => {
+        this.chart_options.legend.selected[labelName] = false
+        var series = {
+            name: labelName,
+            type: "bar",
+            barGap: "0%",
+            barCategoryGap: "10%",
+            data: [],
+            animationDelay: function(idx) {
+              return idx * 0;
+          }
+        }
+        this.chart_options.series.push(series)
+      })
     },
-    drawChart: function() {
-      d3.select("#streamgraph")
-        .datum(this.data)
-        .call(this.chart);
+    addSeriesToChat: function (data, name) {
+      var seriesId = this.chart_options.series.findIndex(x => x.name == name)
+      if(name != "Gesamtheit") {
+        this.local_chart_state.push(name)
+      } else {
+        this.chart_options.xAxis.data = data["time"]
+      }
+      this.chart_options.legend.selected[name] = true
+      this.chart_options.series[seriesId].data = data.data
+    },
+    resetChartToOrigin: async function() {
+      this.removeAllLabels()
+
+      const { data } = await Service.get(`${this.selectEndpoint()}${this.textFilterArg('?')}`);
+      this.addSeriesToChat(data, "Gesamtheit")
+    },
+    updateChart : async function() {
+      var chart_options = this.chart_options
+      var selectEndpoint = this.selectEndpoint
+      var textFilterArg = this.textFilterArg
+      chart_options.series.forEach(async function(x) {
+        if(chart_options.legend.selected[x.name]) {
+          const { data } = await Service.get(`${selectEndpoint()}?label=${x.name}${textFilterArg('&')}`);
+          chart_options.xAxis.data = data["time"]
+          x.data = data.data
+        }
+      })
+    },
+    updateChart_OnLabelChange: async function() {
+      var seriesId = this.chart_options.series.findIndex(x => x.name == 'Gesamtheit')
+      if(seriesId != -1) {
+        this.chart_options.series[seriesId].data = []
+        this.chart_options.legend.selected['Gesamtheit'] = false
+      }
+
+      if(this[Getters.selectedLabels].length > this.local_chart_state.length) {
+          var label = this[Getters.selectedLabels][this[Getters.selectedLabels].length -1]
+          const { data } = await Service.get(`${this.selectEndpoint()}?label=${label}${this.textFilterArg('&')}`);
+          this.addSeriesToChat(data, label)
+      }
+      this.removeDisabledLabels()
+    },
+    textFilterArg: function(prefix) {
+      if(this[Getters.keywordfilter]) {
+        return `${prefix}keyword=${this[Getters.keywordfilter]}`
+      } else {
+        return ""
+      }
+    },
+    selectEndpoint: function() {
+      var endpoint = ''
+      switch(this[Getters.timeFrequency]) {
+        case 'd':
+          endpoint = Endpoint.COMMENTS_GROUP_BY_DAY
+          break;
+        case 'm':
+          endpoint = Endpoint.COMMENTS_GROUP_BY_MONTH
+          break;
+        case 'y':
+          endpoint = Endpoint.COMMENTS_GROUP_BY_YEAR
+          break;
+        default:
+          // code block
+      }
+      return endpoint
+    },
+    removeAllLabels: function() {
+      this.local_chart_state = []
+      this.chart_options.series.forEach(x => x.data = [])
+      for (var key in this.chart_options.legend.selected) {
+        this.chart_options.legend.selected[key] = false // TODO O(1)
+      }
+
+      this[Getters.selectedLabels].forEach(function () {
+        this.chart_options.legend.selected['label'] = false
+      });
+    },
+    removeDisabledLabels: function() {
+      var diff = this.local_chart_state.filter(i =>  this[Getters.selectedLabels].indexOf(i) < 0)
+      while(diff.length > 0) {
+        var labelName = diff.pop()
+        this.chart_options.legend.selected[labelName] = false
+        var seriesId = this.chart_options.series.findIndex(x => x.name == labelName)
+        this.local_chart_state = this.local_chart_state.filter(x => x != labelName)
+        this.chart_options.series[seriesId].data = []
+      }
     }
+  },
+  data() {
+    return {
+      radios: 'm',
+      local_chart_state: [],
+      chart_options: {
+        legend: {
+          align: "left",
+          show: false,
+          selected: {}
+        },
+        toolbox: {
+          feature: {
+            magicType: {
+              type: ["stack", "tiled"]
+            },
+            dataView: {},
+            saveAsImage: {
+              pixelRatio: 2
+            }
+          }
+        },
+        dataZoom: [{
+            type: 'inside'
+        }, {
+            type: 'slider'
+        }],
+        tooltip: {
+          trigger: "axis"
+        },
+        _xAxis: {
+          data: [],
+          silent: false,
+          splitLine: {
+            show: false
+          }
+        },
+        get xAxis() {
+          return this._xAxis;
+        },
+        set xAxis(value) {
+          this._xAxis=value;
+        },
+        yAxis: {},
+        series: [],
+        animationDelayUpdate: function(idx) {
+          return idx * 0;
+        }
+      }
+    };
   }
 };
 </script>
-
-<style type="text/css">
-.path {
-  clip-path: url(#clip);
-}
-</style>
