b02cf7f07d13a23b161dd52dd98c2ee20e0a9b2f
Marlo Haering <haering@informatik.uni-hamburg.de>
2019-08-29 17:52:32 +0200

    fix

--- frontend-demo/src/CommentsUtil.js
+++ frontend-demo/src/CommentsUtil.js
@@ -3,7 +3,7 @@ export const getLabels = (comment, labelId, username) => {
     const labels = comment.labels;
     if (labels) {
         const label = labels.find(label => label.labelId.$oid === labelId);
-        if (label) {
+        if (label && label.manualLabels) {
             // only show annotation of username
             const manualLabel = label.manualLabels.find(ml => ml.annotatorId === username);
             if (manualLabel) {
