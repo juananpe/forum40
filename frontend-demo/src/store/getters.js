fbebba02c67e407ce3e239ea53d2df979ac65af1
Marlo Haering <haering@informatik.uni-hamburg.de>
2019-09-02 14:03:51 +0200

    layout fixes

--- frontend-demo/src/store/getters.js
+++ frontend-demo/src/store/getters.js
@@ -5,7 +5,7 @@ export default {
     [Getters.jwtData]: (state, getters) => state.currentJWT ? JSON.parse(atob(getters.jwt.split('.')[1])) : null,
     [Getters.jwtUser]: (state, getters) => state.currentJWT ? getters.jwtData.user : null,
     [Getters.jwtExpiration]: (state, getters) => getters.jwtData ? getters.jwtData.exp : null,
-    [Getters.jwtLoggedIn]: (state, getters) => getters.jwt && getters.jwtExpiration * 1000 >= state.now,
+    [Getters.jwtLoggedIn]: (state, getters) => !getters.jwt.isEmpty && (getters.jwtExpiration || 0) * 1000 >= state.now,
     [Getters.labelParameters]: (state) => state[State.selectedFilters][State.selectedLabels].map((label) => `label=${label}`).join('&'),
     [Getters.selectedCommentId]: (state) => Object.entries(state.selectedComment).length !== 0 ? state.selectedComment._id.$oid : null,
     [Getters.selectedLabels]: (state) => state[State.selectedFilters][State.selectedLabels],
