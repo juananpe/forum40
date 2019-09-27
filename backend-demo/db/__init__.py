9603be063356e29b9cfc495db5c9cd0494765bb4
Jakob Andersen <jakob.andersen@haw-hamburg.de>
2019-09-27 09:03:25 +0200

    add postgres driver

--- backend-demo/db/__init__.py
+++ backend-demo/db/__init__.py
@@ -1,3 +1,16 @@
 from flask_pymongo import PyMongo
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
+## Mongo DB
 mongo = PyMongo()
+
+## PostgSQL
+PG_HOST = 'postgres'
+PG_PORT = 5432
+PG_DATABASE = 'omp'
+PG_USER = 'postgres'
+PG_PASSWORD = 'postgres'
+
+con = psycopg2.connect(host=PG_HOST, port=PG_PORT, database=PG_DATABASE, user=PG_USER, password=PG_PASSWORD)
+postgres = con.cursor()
