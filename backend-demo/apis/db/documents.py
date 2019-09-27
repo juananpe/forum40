f2e5b18a396fda47e67f72cc3626a873cc57e36d
Jakob Andersen <jakob.andersen@haw-hamburg.de>
2019-09-27 09:11:34 +0200

    port documents API to postgres

--- backend-demo/apis/db/documents.py
+++ backend-demo/apis/db/documents.py
@@ -2,12 +2,17 @@ from flask import request
 from flask_restplus import Resource, reqparse
 
 from apis.db import api
-from db import mongo
+from db import postgres
 
 ns = api.namespace('documents', description="documents api")
 
 @ns.route('/count')
 class DocumentsCount(Resource):
     def get(self):
-        coll = mongo.db.Documents
-        return {'count': coll.find().count()}, 200
+        postgres.execute("SELECT COUNT(*) FROM documents;")
+        db_return = postgres.fetchone()
+
+        if db_return:
+             return {'count': db_return[0]}, 200
+        
+        return {"msg": "Error"}, 400
