9c6e8e2a20aba6be38dc2a185c9cc05e231e71c9
Jakob Andersen <jakob.andersen@haw-hamburg.de>
2019-09-27 09:05:25 +0200

    port user API to postgres

--- backend-demo/apis/db/users.py
+++ backend-demo/apis/db/users.py
@@ -2,13 +2,18 @@ from flask import request
 from flask_restplus import Resource, reqparse
 
 from apis.db import api
-from db import mongo
 
+from db import postgres
 
 ns = api.namespace('users', description="users api")
 
 @ns.route('/count')
 class UsersCount(Resource):
     def get(self):
-        coll = mongo.db.Users
-        return {'count': coll.find().count()}, 200
+        postgres.execute("SELECT COUNT(*) FROM users;")
+        db_return = postgres.fetchone()
+
+        if db_return:
+             return {'count': db_return[0]}, 200
+        
+        return {"msg": "Error"}, 400
