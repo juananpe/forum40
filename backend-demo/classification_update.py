89b908b1a78b5e00e8e514c1b18f1e9c4616eb3c
Marlo Haering <haering@informatik.uni-hamburg.de>
2020-05-06 16:47:07 +0200

    fixe

--- backend-demo/classification_update.py
+++ backend-demo/classification_update.py
@@ -193,7 +193,7 @@ class LabelUpdater(ForumProcessor):
     def initModelTable(self):
         self.cursor.execute(" INSERT INTO model (label_id, timestamp, pid) VALUES(%s, CURRENT_TIMESTAMP, %s) RETURNING id;", (self.label_id, self.pid))
         self.model_entry_id =  self.cursor.fetchone()[0]
-        self.logger.info(f"Init Model Entry: label_id={label_id}, pid={self.pid}")
+        self.logger.info(f"Init Model Entry: label_id={self.label_id}, pid={self.pid}")
 
     def updateModelTable(self, model_details):
         label_id = self.label_id
