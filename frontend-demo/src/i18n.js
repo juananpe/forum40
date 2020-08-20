2b1056c29c0d9ee8b8dd79c0ebda412c7614d41f
Tim Pietz <tim@pietz.me>
2020-08-20 21:14:48 +0200

    Add english translation

--- frontend-demo/src/i18n.js
+++ frontend-demo/src/i18n.js
@@ -1,10 +1,11 @@
 import Vue from "vue";
 import VueI18n from "vue-i18n";
 import de from './locales/de.json';
+import en from './locales/en.json';
 
 Vue.use(VueI18n);
 
-const messages = { de };
+const messages = { de, en };
 
 export default new VueI18n({
 	locale: process.env.VUE_APP_LOCALE,
