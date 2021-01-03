47fc1cddea3b4e0b3541713f6327b8eb209b259e
Tim Pietz <tim@pietz.me>
2021-01-03 20:56:32 +0100

    Make backend not crash if db is temporarily unreachable
    Primarily intended for the first startup during database initialization

--- backend-demo/db/__init__.py
+++ backend-demo/db/__init__.py
@@ -1,2 +1,1 @@
 from db.database import Database, with_database
-from db.connection import postgres_con
--- backend-demo/db/connection.py
+++ backend-demo/db/connection.py
@@ -1,9 +1,47 @@
+import logging
 import psycopg2
+import time
 from psycopg2.pool import ThreadedConnectionPool
+from threading import Lock
+from typing import Optional
 
-# PostgSQL
 from config.settings import PG_HOST, PG_PORT, PG_DATABASE, PG_USER, PG_PASSWORD
+from db.driver import Connection
 
-postgres_con = psycopg2.connect(host=PG_HOST, port=PG_PORT, database=PG_DATABASE, user=PG_USER, password=PG_PASSWORD)
+logger = logging.getLogger(__name__)
 
-db_pool = ThreadedConnectionPool(10, 50, host=PG_HOST, port=PG_PORT, database=PG_DATABASE, user=PG_USER, password=PG_PASSWORD)
+
+class WaitingConnectionPool:
+    def __init__(self, minconn: int, maxconn: int, *args, **kwargs):
+        self.minconn = minconn
+        self.maxconn = maxconn
+        self.args = args
+        self.kwargs = kwargs
+        self._pool: Optional[ThreadedConnectionPool] = None
+        self._lock = Lock()
+
+    def _get_pool(self) -> ThreadedConnectionPool:
+        with self._lock:
+            while self._pool is None or self._pool.closed:
+                try:
+                    self._pool = ThreadedConnectionPool(self.minconn, self.maxconn, *self.args, **self.kwargs)
+                except psycopg2.OperationalError as e:
+                    if e.pgerror is not None:
+                        # not a connection error. Only want to retry if server is currently
+                        # unavailable, not if e.g. password is wrong
+                        raise
+
+                    logger.info('Connection to database failed. Retrying in 3 seconds')
+                    time.sleep(3)
+
+            return self._pool
+
+    def getconn(self) -> Connection:
+        return self._get_pool().getconn()
+
+    def putconn(self, conn: Connection):
+        if self._pool is not None and not self._pool.closed:
+            self._pool.putconn(conn)
+
+
+db_pool = WaitingConnectionPool(10, 50, host=PG_HOST, port=PG_PORT, database=PG_DATABASE, user=PG_USER, password=PG_PASSWORD)
