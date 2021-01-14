d15afd6f8e5d00dd162d579c5c9ca57cec1029f7
Tim Pietz <tim@pietz.me>
2021-01-14 18:54:02 +0100

    Improve permissions on auto generated secrets

--- secrets/generate.sh
+++ secrets/generate.sh
@@ -4,6 +4,7 @@ set -e
 if [ ! -f ./jwt_key ]; then
   echo "Generating JWT key"
   openssl rand -out ./jwt_key 32
+  chmod 600 ./jwt_key
 else
   echo "Using existing JWT key"
 fi
@@ -11,6 +12,7 @@ fi
 if [ ! -f ./db_password.txt ]; then
   echo "Generating database password"
   pwgen -s 32 1 | tr -d '\n' > ./db_password.txt
+  chmod 600 ./db_password.txt
 else
   echo "Using existing database password"
 fi
