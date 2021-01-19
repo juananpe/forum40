3f964d436102c4323793fbf659e6d1c32c3298fd
Tim Pietz <tim@pietz.me>
2021-01-19 13:14:00 +0100

    Add API method to access document of a comment, includes per-paragraph link score

--- backend/db/repositories/documents.py
+++ backend/db/repositories/documents.py
@@ -1,6 +1,6 @@
 import datetime
 
-from typing import TypedDict, Optional
+from typing import TypedDict, Optional, Dict, Set
 
 from db.repositories.base import BaseRepository
 
@@ -15,7 +15,24 @@ class NewDocument(TypedDict):
     metadata: str
 
 
+base_document_fields = frozenset({'id'})
+
+
+def document_fields(content: bool = False, metadata: bool = False, embedding: bool = False) -> Set[str]:
+    fields = set(base_document_fields)
+    if metadata:
+        fields |= {'source_id', 'external_id', 'timestamp', 'category', 'url', 'metadata'}
+    if content:
+        fields |= {'title', 'text', 'markup'}
+    if embedding:
+        fields |= {'embedding'}
+    return fields
+
+
 class DocumentRepository(BaseRepository):
+    def find_by_id(self, id_: int, fields: Set[str] = base_document_fields) -> Dict:
+        return self._acc.fetch_one(f'SELECT {", ".join(fields)} FROM documents WHERE id = %s', (id_,))
+
     def find_id_by_external_id(self, source_id: int, external_id: str) -> Optional[int]:
         return self._acc.fetch_value(
             'SELECT id FROM documents WHERE source_id = %s AND external_id = %s LIMIT 1',
